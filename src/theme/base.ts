64868ba98786413c0d251570b54e5e29d474b515
Stake.Cafe <contact@stake.cafe>
2021-03-22 08:41:40 -0700

    updated theme colors

--- src/theme/base.ts
+++ src/theme/base.ts
@@ -20,12 +20,12 @@ const mediaQueries: MediaQueries = {
 };
 
 export const shadows = {
-  level1: "0px 2px 12px -8px rgba(25, 19, 38, 0.1), 0px 1px 1px rgba(25, 19, 38, 0.05)",
-  active: "0px 0px 0px 1px #0098A1, 0px 0px 4px 8px rgba(31, 199, 212, 0.4)",
-  success: "0px 0px 0px 1px #31D0AA, 0px 0px 0px 4px rgba(49, 208, 170, 0.2)",
-  warning: "0px 0px 0px 1px #ED4B9E, 0px 0px 0px 4px rgba(237, 75, 158, 0.2)",
-  focus: "0px 0px 0px 1px #7645D9, 0px 0px 0px 4px rgba(118, 69, 217, 0.6)",
-  inset: "inset 0px 2px 2px -1px rgba(74, 74, 104, 0.1)",
+  level1: "0px 2px 12px -8px rgba(37, 19, 19, 0.1), 0px 1px 1px rgba(37, 19, 19, 0.05)",
+  active: "0px 0px 0px 1px #a39000, 0px 0px 4px 8px rgba(213, 192, 32, 0.4)",
+  success: "0px 0px 0px 1px #91d679, 0px 0px 0px 4px rgba(115, 237, 74, 0.2)",
+  warning: "0px 0px 0px 1px #e89417, 0px 0px 0px 4px rgba(208, 144, 47, 0.2)",
+  focus: "0px 0px 0px 1px #4e9edf, 0px 0px 0px 4px rgba(69, 150, 217, 0.6)",
+  inset: "inset 0px 2px 2px -1px rgba(104, 89, 74, 0.1)",
 };
 
 const spacing: Spacing = [0, 4, 8, 16, 24, 32, 48, 64];
