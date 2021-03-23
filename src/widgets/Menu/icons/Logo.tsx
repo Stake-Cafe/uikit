e2003a4d3cf7aea01c4b456dea38ca3da8291b37
Stake.Cafe <contact@stake.cafe>
2021-03-22 23:25:14 -0700

    fixed logo merge error

--- src/widgets/Menu/icons/Logo.tsx
+++ src/widgets/Menu/icons/Logo.tsx
@@ -10,11 +10,7 @@ const Logo: React.FC<LogoProps> = ({ isDark, ...props }) => {
   const textColor = isDark ? "#FFFFFF" : "#000000";
   return (
     <Svg viewBox="0 0 205 26" {...props}>
-<<<<<<< HEAD
-      <image width="205" height="26" href={isDark ? "/logos/LogoTextNewDark.png" : "/logos/LogoTextNewWhite.png"} />
-=======
       <image width="205" height="26" href={isDark ? '/egg/LogoTextNewDark.png' : '/egg/LogoTextNewWhite.png'}/>
->>>>>>> 4fb4dd9376e43c4781ce38ab78a72880589389b0
     </Svg>
   );
 };
